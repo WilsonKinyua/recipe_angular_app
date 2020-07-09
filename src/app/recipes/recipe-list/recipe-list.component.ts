a18fcb0292c81ee999900e653a471a1b5f2e88ee
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-07-09 03:20:03 +0300

    upto displaying recipe items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new  Recipe('My Test Recipe','This is a dummy description','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-145969396720.jpg'),
+    new  Recipe('My Test Recipe 2','This is a dummy description 2','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-145969396720.jpg'),
+  ];
   constructor() { }
 
   ngOnInit(): void {
