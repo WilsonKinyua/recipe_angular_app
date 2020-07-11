e54c5b5bd284fcc55469781edb82130e7fea198e
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-07-12 01:54:34 +0300

    working navigation bar with component data sharing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  recipe;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new  Recipe('My Test Recipe','This is a dummy description','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-145969396720.jpg'),
     new  Recipe('My Test Recipe 2','This is a dummy description 2','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-145969396720.jpg'),
+    new  Recipe('My Test Recipe 3','This is a dummy description 3','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-145969396720.jpg'),
   ];
   constructor() { }
 
